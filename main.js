b8eaefaf7b4604667711a326b51f513d0f109df3
Valeh <valeh.mustafayev.98@gmail.com>
2021-12-08 21:18:23 +0100

    accordion desing and open/close logic

---
+++ main.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("accordion-item-arrow-1")
+  .addEventListener("click", () => {
+    document.getElementById("accordion-item-1").classList.toggle("active");
+  });
+document
+  .getElementById("accordion-item-arrow-2")
+  .addEventListener("click", () => {
+    document.getElementById("accordion-item-2").classList.toggle("active");
+  });
+document
+  .getElementById("accordion-item-arrow-3")
+  .addEventListener("click", () => {
+    document.getElementById("accordion-item-3").classList.toggle("active");
+  });
+document
+  .getElementById("accordion-item-arrow-4")
+  .addEventListener("click", () => {
+    document.getElementById("accordion-item-4").classList.toggle("active");
+  });
+document
+  .getElementById("accordion-item-arrow-5")
+  .addEventListener("click", () => {
+    document.getElementById("accordion-item-5").classList.toggle("active");
+  });
